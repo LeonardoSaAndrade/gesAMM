feb55b501a9c6ed30ca6fb69bc98543045bed211
Leonardo <leonardosaandrade.sti2d@gmail.com>
2021-11-26 15:58:09 +0100

    projet
    class en cours

---
+++ gesAMM/gesAMM/Famille.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gesAMM
+{
+    class Famille
+    {
+        private char code;
+        private string libelle;
+        private int nbMediAmm;
+
+        public Famille(char code, string libelle, int nbMediAmm)
+        {
+            this.code = code;
+            this.libelle = libelle;
+            this.nbMediAmm = nbMediAmm;
+        }
+
+        public char getCode() { return this.code; }
+        public string getLibelle() { return this.libelle; }
+        public int getNbMediAmm() { return this.nbMediAmm; }
+    }
+}

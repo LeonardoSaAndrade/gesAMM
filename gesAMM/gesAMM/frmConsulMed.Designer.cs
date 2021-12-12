d07211ceda53f87c9b6423d11c37cffde9b36b65
Lvscls <lilianvscls@gmail.com>
2021-12-12 20:48:35 +0100

    <commit>form consultation

---
+++ gesAMM/gesAMM/frmConsulMed.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace gesAMM
+{
+    partial class frmConsulMed
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvMed = new System.Windows.Forms.ListView();
+            this.lvWorkflow = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lvMed
+            // 
+            this.lvMed.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.lvMed.HideSelection = false;
+            this.lvMed.Location = new System.Drawing.Point(51, 85);
+            this.lvMed.Name = "lvMed";
+            this.lvMed.Size = new System.Drawing.Size(476, 349);
+            this.lvMed.TabIndex = 0;
+            this.lvMed.UseCompatibleStateImageBehavior = false;
+            this.lvMed.View = System.Windows.Forms.View.Details;
+            // 
+            // lvWorkflow
+            // 
+            this.lvWorkflow.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8});
+            this.lvWorkflow.HideSelection = false;
+            this.lvWorkflow.Location = new System.Drawing.Point(616, 87);
+            this.lvWorkflow.Name = "lvWorkflow";
+            this.lvWorkflow.Size = new System.Drawing.Size(541, 346);
+            this.lvWorkflow.TabIndex = 1;
+            this.lvWorkflow.UseCompatibleStateImageBehavior = false;
+            this.lvWorkflow.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Depot Légal";
+            this.columnHeader1.Width = 138;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Nom commercial";
+            this.columnHeader2.Width = 158;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "n° étape";
+            this.columnHeader3.Width = 87;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "libellé";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "date de décision";
+            this.columnHeader5.Width = 82;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "libellé de la décision";
+            this.columnHeader6.Width = 149;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Norme";
+            this.columnHeader7.Width = 75;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Date de la norme";
+            this.columnHeader8.Width = 148;
+            // 
+            // frmConsulMed
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1169, 535);
+            this.Controls.Add(this.lvWorkflow);
+            this.Controls.Add(this.lvMed);
+            this.Name = "frmConsulMed";
+            this.Text = "frmConsulMed";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvMed;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ListView lvWorkflow;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+    }
+}
